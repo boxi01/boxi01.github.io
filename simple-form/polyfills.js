6fbea7079849e87118a341a9473308dba5150cff
jpude <joannapudelko95@gmail.com>
2021-11-02 21:37:33 +0100

    Adding bootstrap nav bar

--- simple-form/polyfills.js
+++ simple-form/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\jpude\OneDrive\Pulpit\PROJEKTY\Simple-form.github.io\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\jpude\OneDrive\Pulpit\PROJEKTY\Simple-form\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
